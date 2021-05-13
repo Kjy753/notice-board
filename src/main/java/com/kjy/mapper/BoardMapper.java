64d4cefdee614ef68d601737aaefa1189e4eac1f
kjy <aty753@naver.com>
2021-05-13 17:42:06 +0900

    BovardVO 작성 및 mapper 인터페이스 를 이용해 list 출력 테스트

---
+++ src/main/java/com/kjy/mapper/BoardMapper.java
@@ -0,0 +1,13 @@
+package com.kjy.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.kjy.domain.BoardVO;
+
+public interface BoardMapper {
+
+	@Select("select * from tbl_board where bno > 0")
+	public List<BoardVO> getList();
+}
--- src/test/java/com/kjy/mapper/BoardMapperTests.java
+++ src/test/java/com/kjy/mapper/BoardMapperTests.java
@@ -6,22 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
-public class TimeMapperTests {
+public class BoardMapperTests {
 	
-	@Autowired
-	private TimeMapper timeMapper;
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
 	
 	@Test
-	public void testTime() {
-		
-		log.info("testTiem......"+timeMapper.getTime());
-	}
+	public void testGetList() {
 	
+		mapper.getList().forEach(board -> log.info(board));
+	}
+
 
 }
