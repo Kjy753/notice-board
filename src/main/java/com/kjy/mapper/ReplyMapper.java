3b509079140f5dc35b43a315292370b3c5d80867
kjy <aty753@naver.com>
2021-06-18 12:31:16 +0900

    50개의 게시글에 1개씩 댓글 새로 생성하는 Create 작업

--- src/main/java/com/kjy/mapper/ReplyMapper.java
+++ src/main/java/com/kjy/mapper/ReplyMapper.java
@@ -1,5 +1,10 @@
 package com.kjy.mapper;
 
+import com.kjy.domain.ReplyVO;
+
 public interface ReplyMapper {
+	
+	// 댓글 등록
+	public int insert(ReplyVO vo);
 
 }
--- src/test/java/com/kjy/mapper/ReplyMapperTests.java
+++ src/test/java/com/kjy/mapper/ReplyMapperTests.java
@@ -1,10 +1,14 @@
 package com.kjy.mapper;
 
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.kjy.domain.ReplyVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -13,6 +17,9 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class ReplyMapperTests {
+	
+	/* 테스트를 위해 미리 찾아본 게시글 번호 배열 */
+	private Long[] bnoArr = {40L, 38L, 37L, 36L, 35L, 34L };
 
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
@@ -22,5 +29,21 @@ public class ReplyMapperTests {
 		log.info("================");
 		log.info(mapper);
 		log.info("================");
+	}
+	
+	@Test
+	public void testCreate() {
+		
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+			
+			ReplyVO vo = new ReplyVO();
+			
+			// 게시물의 번호
+			vo.setBno(bnoArr[i %5]);
+			vo.setReply("댓클 테스트");
+			vo.setReplyer("replyer" + i);
+			
+			mapper.insert(vo);
+		});
 	}
 }
