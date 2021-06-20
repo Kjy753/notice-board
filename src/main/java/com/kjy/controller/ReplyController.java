e921250514cac24d7a05c4a103abbe5436bc0c7b
kjy <aty753@naver.com>
2021-06-20 17:12:13 +0900

    특정 게시물 댓글 목록 조회 작업

--- src/main/java/com/kjy/controller/ReplyController.java
+++ src/main/java/com/kjy/controller/ReplyController.java
@@ -1,13 +1,18 @@
 package com.kjy.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kjy.domain.Criteria;
 import com.kjy.domain.ReplyVO;
 import com.kjy.service.ReplyService;
 
@@ -36,5 +41,19 @@ public class ReplyController {
 		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	@GetMapping(value = "/pages/{bno}/{page}",
+			produces = {
+					MediaType.APPLICATION_ATOM_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(
+			@PathVariable("page") int page,
+			@PathVariable("bno") Long bno){
+		log.info("get List.............");
+		Criteria cri = new Criteria(page,10);
+		log.info(cri);
+		
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+		
+	}
 	
 }
