d84d9751adb62cb64fad241ac8c56ee0be02346e
kjy <aty753@naver.com>
2021-06-20 16:33:43 +0900

    json 관련 라이브러리 추가 및 등록 작업 처리

--- src/main/java/com/kjy/controller/ReplyController.java
+++ src/main/java/com/kjy/controller/ReplyController.java
@@ -1,8 +1,14 @@
 package com.kjy.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kjy.domain.ReplyVO;
 import com.kjy.service.ReplyService;
 
 import lombok.AllArgsConstructor;
@@ -15,5 +21,20 @@ import lombok.extern.log4j.Log4j;
 public class ReplyController {
 	// 생성자 주입을 통해 serviceImpl 객체인 service 주입
 	private ReplyService service;
+	
+	@PostMapping(value = "/new",
+			consumes = "application/json", /* 읽어드릴수 있는 타입 명시 */
+			produces = { MediaType.TEXT_PLAIN_VALUE }) /* 만들어낼 수 있는 타입의 목록을 열거 */
+	public ResponseEntity<String> create(@RequestBody ReplyVO vo){
+		
+		log.info("ReplyVO: " + vo);
+		
+		int insertCount = service.register(vo);
+		
+		log.info("Reply INSERT COUNT: " + insertCount);
+		
+		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 	
 }
