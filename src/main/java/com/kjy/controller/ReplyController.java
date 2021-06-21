4ca41ae5537ea1bafb61b120f4fe16020d742195
kjy <aty753@naver.com>
2021-06-21 11:29:32 +0900

    댓글 수정 작업

--- src/main/java/com/kjy/controller/ReplyController.java
+++ src/main/java/com/kjy/controller/ReplyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kjy.domain.Criteria;
@@ -26,7 +27,8 @@ import lombok.extern.log4j.Log4j;
 public class ReplyController {
 	// 생성자 주입을 통해 serviceImpl 객체인 service 주입
 	private ReplyService service;
-	
+
+	/* 댓글 등록 */
 	@PostMapping(value = "/new",
 			consumes = "application/json", /* 읽어드릴수 있는 타입 명시 */
 			produces = { MediaType.TEXT_PLAIN_VALUE }) /* 만들어낼 수 있는 타입의 목록을 열거 */
@@ -40,7 +42,8 @@ public class ReplyController {
 		
 		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
+
+	/* 댓글 목록 출력 */
 	@GetMapping(value = "/pages/{bno}/{page}",
 			produces = {
 					MediaType.APPLICATION_ATOM_XML_VALUE,
@@ -55,5 +58,21 @@ public class ReplyController {
 		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
 		
 	}
+	
+	/* 댓글 수정 */
+	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH},
+			value = "/{rno}",
+			consumes = "application/json",
+			produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo,
+										 @PathVariable("rno") Long rno){
+		vo.setRno(rno);
+		log.info("rno : "+ rno);
+		
+		log.info("modify : "+ vo);
+		
+		return service.modify(vo) == 1 ? new ResponseEntity<>("success",HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 	
 }
