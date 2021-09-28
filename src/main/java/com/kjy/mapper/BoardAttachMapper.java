4f416c22d844746ac523324aa355555da06d27b8
kjy <aty753@naver.com>
2021-09-28 14:26:33 +0900

    mapper 작성

---
+++ src/main/java/com/kjy/mapper/BoardAttachMapper.java
@@ -0,0 +1,16 @@
+package com.kjy.mapper;
+
+import java.util.List;
+
+import com.kjy.domain.BoardAttachVO;
+
+// 첨부파일 정보 관련 메퍼
+public interface BoardAttachMapper {
+
+	// 추가
+	public void insert(BoardAttachVO vo);
+	//삭제
+	public void delete(String uuid);
+	// 특정 게시물번호로 찾기
+	public List<BoardAttachVO> findByBno(Long bno);
+}
