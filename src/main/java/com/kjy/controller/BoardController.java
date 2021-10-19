8e4a542b902de7fc6d84999ec46dd5d1f2316458
kjy <aty753@naver.com>
2021-10-19 15:29:15 +0900

    게시물 삭제 및 파일 삭제

--- src/main/java/com/kjy/controller/BoardController.java
+++ src/main/java/com/kjy/controller/BoardController.java
@@ -1,5 +1,8 @@
 package com.kjy.controller;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -104,7 +107,12 @@ public class BoardController {
 		
 		log.info("remove :" + bno);
 		
+		List<BoardAttachVO> attachList = service.getAttachList(bno);
+		
 		if(service.remove(bno)) {
+			
+			// deltet Attach FIles
+			deleteFiles(attachList);
 			rttr.addFlashAttribute("result","success");
 		}
 		
@@ -124,6 +132,34 @@ public class BoardController {
 		log.info("getAttachList " + bno);
 		
 		return new ResponseEntity<>(service.getAttachList(bno),HttpStatus.OK);
+	}
+	
+	//파일 삭제메소드
+	private void deleteFiles(List<BoardAttachVO> attachList) {
+		
+		if(attachList == null || attachList.size() == 0) {
+			return;
+		}
+		
+		log.info("delete attach files ...........");
+		log.info(attachList);
+		
+		attachList.forEach(attach -> {
+			try {
+				Path file = Paths.get("D:\\upload\\"+attach.getUploadPath()+"\\"+ attach.getUuid()+"_"+ attach.getFileName());
+				
+				Files.deleteIfExists(file);
+				
+				if(Files.probeContentType(file).startsWith("image")) {
+					
+					Path thumbNail = Paths.get("D:\\upload\\"+attach.getUploadPath()+"\\s_" + attach.getUuid()+ "_" + attach.getFileName());
+					
+					Files.delete(thumbNail);
+				}
+			}catch(Exception e) {
+				log.error("delete file error" + e.getMessage());
+			}//end catch
+		});
 	}
 	
 }
