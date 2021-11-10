02574372bf6fbbee0b0014681dfef242b71647d5
kjy <aty753@naver.com>
2021-11-10 20:20:21 +0900

    BcryptPasswordEncoder 빈 등록 및 테스트 클래스 생성

---
+++ src/test/java/com/kjy/security/MemberTests.java
@@ -0,0 +1,16 @@
+package com.kjy.security;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.kjy.service.BoardServiceTests;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml","file:src/main/webapp/WEB-INF/spring/security-context.xml"})
+@Log4j
+public class MemberTests {
+
+}
